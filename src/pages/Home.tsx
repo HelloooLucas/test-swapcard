63a86265e7cf2443f8460fa76e500c10804dbe65
Lucas Montenoise <montenoise.lucas@gmail.com>
2020-10-05 16:25:08 +0200

    Homepage query is now typed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
 
+interface Artist {
+    id: string;
+    name: string;
+}
+
+interface SearchResult {
+    search: {
+        artists: {
+            nodes: Artist[]
+        }
+    }
+};
+
+interface SearchArtistVars {
+    query: string;
+};
+
 const SEARCH_ARTIST = gql`
     query Artists ($query: String!) {
         search {
@@ -17,8 +34,9 @@ const SEARCH_ARTIST = gql`
 
 const Home:React.FC = () => {
     const [query, setQuery] = useState('');
-    const { loading, error, data } = useQuery(SEARCH_ARTIST, { variables: { query }});
+    const { loading, error, data } = useQuery<SearchResult, SearchArtistVars>(SEARCH_ARTIST, { variables: { query }});
     const artists = data?.search.artists.nodes;
+    useEffect(() => console.log(artists))
 
     return (
         <>
@@ -35,7 +53,7 @@ const Home:React.FC = () => {
              : error
              ? <p>Error :(</p>
              : <ul>
-                 {artists.map((artist: any) => (
+                 {artists?.map((artist: any) => (
                      <li key={artist.id}>
                          <h3>{artist.name}</h3>
                          <NavLink to={`/artists/${artist.name}`}>Go to artist</NavLink>
