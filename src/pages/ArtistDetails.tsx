0a6efae0643b0b486d75d707f9f64371fa83008d
Lucas Montenoise <montenoise.lucas@gmail.com>
2020-10-05 19:09:54 +0200

    Artist details page fetching works properly

--- src/pages/ArtistDetails.tsx
+++ src/pages/ArtistDetails.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import { RouteComponentProps } from 'react-router-dom';
 
-const ArtistDetails:React.FC<RouteComponentProps> = ({ history }) => {
+const FIND_ARTIST = gql`
+    query SingleArtist ($id: ID!) {
+        node(id: $id) {
+            ...on Artist {
+                name
+                releases {
+                    nodes {
+                        title
+                        coverArtArchive {
+                            front
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
+
+interface MatchProp {
+    id: string;
+};
+
+interface ArtistDetailsProps extends RouteComponentProps<MatchProp> {};
+
+const ArtistDetails:React.FC<ArtistDetailsProps> = ({ history, match }) => {
+    const artistId = match.params.id;
+    const { loading, error, data } = useQuery(FIND_ARTIST, { variables: { id: artistId }})
+    const artist = data?.node;
+    
     return (
         <>
             <button onClick={history.goBack}>Back to list</button>
-            <div>Artist Details</div>
+            <h3>Artist Details</h3>
+            {loading
+             ? <p>Loading...</p>
+             : error
+             ? <p>Error :(</p>
+             : <div>
+                 <h1>{artist.name}</h1>
+                 <ul>
+                     {artist.releases.nodes.map((release: any) => release.title)}
+                 </ul>
+             </div>
+            }
         </>
     );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { NavLink } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
+import { NavLink } from 'react-router-dom';
 import debounce from 'lodash.debounce';
 
 import { SearchBar } from './../components';
@@ -22,7 +22,7 @@ interface SearchArtistVars {
     query: string;
 };
 
-const SEARCH_ARTIST = gql`
+const SEARCH_ARTISTS = gql`
     query Artists ($query: String!) {
         search {
             artists(query: $query) {
@@ -37,15 +37,15 @@ const SEARCH_ARTIST = gql`
 
 const Home:React.FC = () => {
     const [query, setQuery] = useState('');
-    const debouncedOnChange = useCallback(debounce(inputText => setQuery(inputText), 400), []);
+    const debouncedSetQuery = useCallback(debounce(inputText => setQuery(inputText), 400), []);
 
-    const { loading, error, data } = useQuery<SearchResult, SearchArtistVars>(SEARCH_ARTIST, { variables: { query }});
+    const { loading, error, data } = useQuery<SearchResult, SearchArtistVars>(SEARCH_ARTISTS, { variables: { query }});
     const artists = data?.search.artists.nodes;
 
     return (
         <>
             <h1>HOME</h1>
-            <SearchBar onChange={debouncedOnChange} />
+            <SearchBar onChange={debouncedSetQuery} />
             {!query
              ? <p>Please make a search</p>
              : loading
@@ -56,7 +56,7 @@ const Home:React.FC = () => {
                  {artists?.map((artist: any) => (
                      <li key={artist.id}>
                          <h3>{artist.name}</h3>
-                         <NavLink to={`/artists/${artist.name}`}>Go to artist</NavLink>
+                         <NavLink to={`/artists/${artist.id}`}>Go to artist</NavLink>
                      </li>
                  ))}
              </ul>
